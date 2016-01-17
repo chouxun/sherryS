a853b03a2e8ec4111b7d3e002b82d1c63981ac95
chouxun <chou.xun@qq.com>
2016-01-17 23:19:03 +0800

    Initial Commit

---
+++ 共享/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  共享
+//
+//  Created by 仇寻 on 16/1/17.
+//  Copyright © 2016年 st. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
